7d6cbce0fbf782e72dd69171bdc365a904b2429f
Pawan Kumar <pawank@xiaomi.com>
2022-04-24 07:53:49 +0530

    Setting up the project

---
+++ app/src/main/java/start/gopay/gojek/MainActivity.kt
@@ -0,0 +1,12 @@
+package start.gopay.gojek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    // Splash Activity 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
